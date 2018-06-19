49cdfe330e8df80f119104f0c1af0936170f13e4
kentaro0113 <kentaro.t.work0113@gamil.com>
2018-06-19 13:49:22 +0900

    first test

---
+++ app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
@@ -0,0 +1,14 @@
+package chibafes.com.a56thchibafes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+    }
+
+}
---
+++ app/src/main/java/chibafes/com/a56thchibafes/Statics.java
@@ -0,0 +1,5 @@
+package chibafes.com.a56thchibafes;
+
+public class Statics {
+    public static final int NONE    = -1;
+}
