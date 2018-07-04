fab3e66c8fd3d1dc17cf2b7f32bfbc123d2764bb
foura <fourazm@gmail.com>
2018-07-04 20:41:20 +0900

    -1を-5に変えた

--- app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
+++ app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package chibafes.com.a56thchibafes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,6 +13,16 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        ImageButton sendButton = findViewById(R.id.button_ex);
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplication(), KikakuSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
