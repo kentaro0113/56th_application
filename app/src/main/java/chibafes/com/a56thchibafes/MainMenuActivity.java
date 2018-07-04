e8345028ca409de44dc8e26cfaeb0ab644ef26b4
kentaro0113 <kentaro.t.work0113@gamil.com>
2018-07-04 20:40:45 +0900

    定数宣言の変更

--- app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
+++ app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
@@ -1,7 +1,10 @@
 package chibafes.com.a56thchibafes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        ImageButton sendButton = findViewById(R.id.button_ex);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), HappiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
