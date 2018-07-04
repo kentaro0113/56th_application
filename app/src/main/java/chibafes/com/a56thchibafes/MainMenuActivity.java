2b4c1cbe9540889509451cce3aa82b2753f9e1be
suzukiayano <suzukiayano916@gmail.com>
2018-07-04 20:41:15 +0900

    to-5from-1

--- app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
+++ app/src/main/java/chibafes/com/a56thchibafes/MainMenuActivity.java
@@ -1,14 +1,28 @@
 package chibafes.com.a56thchibafes;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainmenu);
-    }
 
+        setContentView(R.layout.activity_mainmenu);
+        ImageButton sendButton = findViewById(R.id.imageButton_ex);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), HappiActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
+
