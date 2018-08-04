ede02ee4c94164864a0be499324a45cd39f6d193
foura <fourazm@gmail.com>
2018-08-04 15:54:13 +0900

    企画アクティビティ初期化

---
+++ app/src/main/java/chibafes/com/a56thchibafes/KikakuSearchActivity.java
@@ -0,0 +1,16 @@
+package chibafes.com.a56thchibafes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class KikakuSearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+
+    }
+}
