f3390593b4bbc16bfea6795edcdb552dd18251cc
kentaro0113 <kentaro.t.work0113@gamil.com>
2019-10-16 21:14:01 +0900

    定数宣言にコメント

--- app/src/main/java/com/chibafes/chibafes56/Statics.java
+++ app/src/main/java/com/chibafes/chibafes56/Statics.java
@@ -7,9 +7,17 @@ package com.chibafes.chibafes56;
  */
 
 public class Statics {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //この下の"http://demo.chibafes.com/appli/php/○○/～～～～.php"の○○は開催する回の序数(第55回→55th, 第57回→57th)を埋める！！
+
     static final String URL_UPDATE       = "http://demo.chibafes.com/appli/php/57th/update.php";
     static final String URL_ENQUETE      = "http://demo.chibafes.com/appli/php/57th/enquete_answer.php";
     static final String URL_CHIBAFES_WEB = "http://chibafes.com/";
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //これ以降の文は変えない！
 
     static final int NONE                = -1;
     public static final int LIMIT_COUNT_NO      = 99999; // 各リストの最大index値（保険）
